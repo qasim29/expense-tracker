2659f06a60e9327d48d06d686373c5921953424a
qasim.29 <qasimmohammad534@gmail.com>
2022-12-26 19:31:38 +0500

    tansaction component added

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,8 +3,8 @@ import "./Transaction.css";
 function Transaction(Prop) {
   return (
     <div className="transaction">
-      <h4>Prop.name</h4>
-      <h4>Prop.ammount</h4>
+      <h4 id="name">Prop.name</h4>
+      <h4 id="ammount">Prop.ammount</h4>
     </div>
   );
 }
